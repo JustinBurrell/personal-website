4577bdce6f4e064d9ba51743fade43ffb8495f82
Justin Burrell <justinburrell715@gmail.com>
2025-06-08 18:16:04 -0400

    Fixed path error due to component folder fix

--- personal-portfolio/src/components/Experience.js
+++ personal-portfolio/src/components/Experience.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import SectionTitle from '../../assets/ui/SectionTitle';
-import Card from '../../assets/ui/Card';
-import Button from '../../assets/ui/Button';
-import portfolioData from '../../data/portfolioData.ts';
+import SectionTitle from '../assets/ui/SectionTitle';
+import Card from '../assets/ui/Card';
+import Button from '../assets/ui/Button';
+import portfolioData from '../data/portfolioData.ts';
 
 const Experience = () => {
   return (
